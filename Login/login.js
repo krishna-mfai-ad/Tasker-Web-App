054a106dab66a887e9f3dc6c996cd1ff9b3e7111
Krishna R <164004602+krishna-mfai-ad@users.noreply.github.com>
2024-05-19 21:12:10 +0530

    Add files via upload

--- Login/login.js
+++ Login/login.js
@@ -1,30 +1,22 @@
-var email=document.getElementById("emailID")
-var pass=document.getElementById("password")
+// var mail = document.getElementById("emailID")
+// var pass = document.getElementById("password")
 
-var emailStatus=document.getElementById("emailStatus")
-var passStatus=document.getElementById("passStatus")
-function signing(){
-    let index="http://127.0.0.1:5500/Home/home.html"
-    if(email.value=="krishna@mfedu.com" && pass.value=="12345678"){
-        window.location.assign(index)
+// var emailStatus = document.getElementById("emailStatus")
+// var passStatus = document.getElementById("passStatus")
+
+function signing() {
+    let email, password;
+    email = document.getElementById("emailID").value;
+    password = document.getElementById("password").value;
+
+    let userRecords = new Array();
+    userRecords = JSON.parse(localStorage.getItem("users")) || []
+    if (userRecords.some((v) => {
+        return v.email == email && v.password == password
+    })) {
+        window.location.assign("http://127.0.0.1:5500/Home/home.html");
     }
-    else{
-        console.log("Else")
-        if(email.value!="krishna@mfedu.com"){
-            emailStatus.innerText="Incorrect Mail"
-            emailStatus.style="color:red; text-align:end;"
-        }
-        else{
-            emailStatus.innerText="Mail Accepted"
-            emailStatus.style="color:green; text-align:end;"
-        }
-        if(pass.value!="12345678"){
-            passStatus.textContent="Incorrect Password"
-            passStatus.style="color:red; text-align:end"
-        }
-        else{
-            passStatus.style="color:green; text-align:end;"
-            passStatus.innerText="Password Accepted"
-        }
+    else {
+        alert("Email or Password is not matching");
     }
 }
